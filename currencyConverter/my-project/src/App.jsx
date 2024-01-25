8496d8a05d0719b11faf8a8c04e064843547ac82
Bijay B.K <bijaybk307@gmail.com>
2024-01-26 01:51:25 +0545

    hooks initial comit

---
+++ currencyConverter/my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-3xl text-black bg-blue-500'>Currency Converter App</h1>
+    </>
+  )
+}
+
+export default App
